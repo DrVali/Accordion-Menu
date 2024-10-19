1bf6f141a0971845112b76a1af9ea2db8b089575
Ali Ahmadi <ahmadi.world@gmail.com>
2024-10-19 10:20:21 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const myAcc = document.getElementsByClassName("accordion-header");
+let mySpan =document.getElementsByClassName("accardionSpan");
+
+for (let i = 0; i < myAcc.length; i++) {
+	myAcc[i].addEventListener("click", function() {
+	 for(let j=0;j<myAcc.length;j++){
+		if(i != j){
+		let allOpen=myAcc[j].nextElementSibling;
+		allOpen.style.display = "none";
+		mySpan[j].innerHTML="+";
+		}
+	}
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+	  mySpan[i].innerHTML="+";
+	  
+    } else {
+      panel.style.display = "block";
+	  mySpan[i].innerHTML="-";
+	  
+    }
+  });
+}
+
